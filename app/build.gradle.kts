c0a416912946feba19c578c7a0c8fb39a6ea23b0
Ricardo790 <ricardo177890@gmail.com>
2024-05-07 23:27:19 -0600

    Se hizo la prueba de splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,12 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.airbnb.android:lottie:6.4.0")
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
 }
