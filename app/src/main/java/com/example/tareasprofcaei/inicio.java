9f8bc39a6e56d8093b90e1c56a0d35b70c395994
Ricardo790 <ricardo177890@gmail.com>
2024-05-10 00:45:28 -0600

    terminado de las caracteristicas

--- app/src/main/java/com/example/tareasprofcaei/inicio.java
+++ app/src/main/java/com/example/tareasprofcaei/inicio.java
@@ -1,12 +1,16 @@
 package com.example.tareasprofcaei;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class inicio extends AppCompatActivity {
 
@@ -20,5 +24,18 @@ public class inicio extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        GifImageView botonImagen = findViewById(R.id.botonCrear);
+        botonImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí puedes realizar las acciones que deseas cuando se haga clic en el botón de imagen
+                // Por ejemplo, iniciar una nueva actividad
+                Intent intent = new Intent(inicio.this, crearTarea.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
     }
 }
